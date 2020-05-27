70a32c70c6cd5e1c776e48a13a8d88e1fc5e74a0
Zixu_Wang <Zixu_Wang@asus.com>
2020-05-27 16:36:46 +0800

    Implement Img2ColorfulChars.

---
+++ Img2ColorfulChars/NativeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Img2ColorfulChars
+{
+    internal class NativeMethods
+    {
+        // Source: visual studio - Custom text color in C# console application? - Stack Overflow
+        // Answered by Alexei Shcherbakov & Olivier Jacot-Descombes
+        // https://stackoverflow.com/questions/7937256/custom-text-color-in-c-sharp-console-application
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetConsoleMode(IntPtr handle, out int mode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(int handle);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Img2ColorfulChars
 Load image and print colorful chars in C# console.
+
+## Usage
+1. Build this project in Visual Studio or use Img2ColorfulChars.exe in _Output.
+2. Run command ```Img2ColorfulChars.exe ImagePath Scale``` and enjoy your colorful chars.  
+``` cmd
+cd _Output
+Img2ColorfulChars.exe np.png 4
+``` 
+> Notice: Scale should be positive integar, and larger scale gets smaller image.  
+
+## Output
+Original image:  
+<img width="400" height="400" src="Images/original.png"/>  
+
+Output:  
+<img width="400" height="400" src="Images/output.png"/>  
+
