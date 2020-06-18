1adf4a25b51ee47d4ffcfe4005a2a833817f88b3
Zixu_Wang <Zixu_Wang@asus.com>
2020-06-18 13:10:18 +0800

    Add Simplified Chinese to ScaleBox.

--- Img2ColorfulChars/ScaleBox.Designer.cs
+++ Img2ColorfulChars/ScaleBox.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScaleBox));
             this.tb_Scale = new System.Windows.Forms.TextBox();
             this.btn_Set = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -36,61 +37,40 @@
             // 
             // tb_Scale
             // 
-            this.tb_Scale.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.tb_Scale.Location = new System.Drawing.Point(14, 14);
-            this.tb_Scale.MaxLength = 4;
-            this.tb_Scale.Multiline = true;
+            resources.ApplyResources(this.tb_Scale, "tb_Scale");
             this.tb_Scale.Name = "tb_Scale";
-            this.tb_Scale.Size = new System.Drawing.Size(176, 26);
-            this.tb_Scale.TabIndex = 0;
-            this.tb_Scale.WordWrap = false;
             this.tb_Scale.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_Scale_KeyPress);
             // 
             // btn_Set
             // 
-            this.btn_Set.Location = new System.Drawing.Point(196, 14);
+            resources.ApplyResources(this.btn_Set, "btn_Set");
             this.btn_Set.Name = "btn_Set";
-            this.btn_Set.Size = new System.Drawing.Size(61, 26);
-            this.btn_Set.TabIndex = 1;
-            this.btn_Set.Text = "Set";
             this.btn_Set.UseVisualStyleBackColor = true;
             this.btn_Set.Click += new System.EventHandler(this.btn_Set_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 64);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 30);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "1. Scale should be positive integer.\r\n2. Larger scale gets smaller image.";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            resources.ApplyResources(this.label2, "label2");
             this.label2.ForeColor = System.Drawing.Color.Green;
-            this.label2.Location = new System.Drawing.Point(11, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(246, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Given is suggested scale (no need to scroll).";
             // 
             // ScaleBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 105);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Set);
             this.Controls.Add(this.tb_Scale);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ScaleBox";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Set scale";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.ScaleBox_Load);
             this.ResumeLayout(false);
